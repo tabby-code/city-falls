52e3f73cd77e70932acf9a5e4d9436d361fc3dbc
tabby-code <joannalime09@gmail.com>
2025-11-15 10:34:34 -0500

    c

---
+++ nicole_idle.gd
@@ -0,0 +1,21 @@
+extends CharacterBody2D
+
+@export var speed =300.0
+const jump=-400.0
+@export_range(0,1) var deceleration=0.1
+
+var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
+
+func _physics_process(delta):
+	if not is_on_floor():
+		velocity.y+= gravity * delta
+	
+	if Input.is_action_just_pressed("jump") and is_on_floor():
+		velocity.y= jump
+	
+	var direction= Input.get_axis("left", "right")
+	if direction:
+		velocity.x=direction * speed
+	else:
+		velocity.x=move_toward(velocity.x, 0, speed)
+	move_and_slide()
